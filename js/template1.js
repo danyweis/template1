08dfe07f2aa182d5c524ba3dc0685b5b92fa1443
danyweis <danyweismail@gmail.com>
2016-04-26 15:02:55 +0200

    fini tout fonctione maintenant il faut just voire comment le mettre sur mon propre site

--- js/template1.js
+++ js/template1.js
@@ -12,15 +12,81 @@ $(function(){
     $bgBL = $('.bgBL');
     $bottomRight = $('#bottomRight');
     $material = $('.material');
-    $bgBR = $('bgBR');
+    $bgBR = $('.bgBR');
+
+    $home.on('click',function(){
+        $topLeft.animate({height: '48%', width: '48%'},1500);
+        $bottomLeft.animate({height: '48%', width: '48%'},1500);
+        $topRight.animate({height: '48%', width: '48%'},1500);
+        $bottomRight.animate({height: '48%', width: '48%'},1500);
+        $histoir.fadeIn(1500);
+        $bgTL.fadeOut(1500);
+        $paysage.fadeIn(1500);
+        $bgTR.fadeOut(1500);
+        $today.fadeIn(1500);
+        $bgBL.fadeOut(1500);
+        $material.fadeIn(1500);
+        $bgBR.fadeOut(1500);
+    });
 
     $topLeft.on('click', function(){
         $topLeft.animate({height: '90%', width: '90%'},1500);
         $bottomLeft.animate({height: '8%', width: '8%'},1500);
         $topRight.animate({height: '8%', width: '8%'},1500);
         $bottomRight.animate({height: '8%', width: '8%'},1500);
-        $histoir.css('background-image', 'initial');
-        $bgTL.css('display', 'initial');
+        $histoir.fadeOut(1500);
+        $bgTL.fadeIn(1500);
+        $paysage.fadeIn(1500);
+        $bgTR.fadeOut(1500);
+        $today.fadeIn(1500);
+        $bgBL.fadeOut(1500);
+        $material.fadeIn(1500);
+        $bgBR.fadeOut(1500);
+
+    });
+    $topRight.on('click', function(){
+        $topRight.animate({height: '90%', width: '90%'},1500);
+        $bottomLeft.animate({height: '8%', width: '8%'},1500);
+        $topLeft.animate({height: '8%', width: '8%'},1500);
+        $bottomRight.animate({height: '8%', width: '8%'},1500);
+        $paysage.fadeOut(1500);
+        $bgTR.fadeIn(1500);
+        $histoir.fadeIn(1500);
+        $bgTL.fadeOut(1500);
+        $today.fadeIn(1500);
+        $bgBL.fadeOut(1500);
+        $material.fadeIn(1500);
+        $bgBR.fadeOut(1500);
+
+    });
+    $bottomLeft.on('click', function(){
+        $bottomLeft.animate({height: '90%', width: '90%'},1500);
+        $topLeft.animate({height: '8%', width: '8%'},1500);
+        $topRight.animate({height: '8%', width: '8%'},1500);
+        $bottomRight.animate({height: '8%', width: '8%'},1500);
+        $today.fadeOut(1500);
+        $bgBL.fadeIn(1500);
+        $paysage.fadeIn(1500);
+        $bgTR.fadeOut(1500);
+        $histoir.fadeIn(1500);
+        $bgTL.fadeOut(1500);
+        $material.fadeIn(1500);
+        $bgBR.fadeOut(1500);
+
+    });
+    $bottomRight.on('click', function(){
+        $bottomRight.animate({height: '90%', width: '90%'},1500);
+        $bottomLeft.animate({height: '8%', width: '8%'},1500);
+        $topLeft.animate({height: '8%', width: '8%'},1500);
+        $topRight.animate({height: '8%', width: '8%'},1500);
+        $material.fadeOut(1500);
+        $bgBR.fadeIn(1500);
+        $histoir.fadeIn(1500);
+        $bgTL.fadeOut(1500);
+        $today.fadeIn(1500);
+        $bgBL.fadeOut(1500);
+        $paysage.fadeIn(1500);
+        $bgTR.fadeOut(1500);
 
     });
 
